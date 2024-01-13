194830df0c1b09c7fb1e5455bc4215719cfa4362
Raj <121766917+RK02k@users.noreply.github.com>
2024-01-14 00:08:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // For intialzing we have to first initialize the express by setting up npm init then npm i express
 const express = require('express')
-const path = require('path') // core module which cant be loaded by npm i 
+const path = require('path') 
 const http = require('http')
 
-const socketio = require('socket.io') //we get a fundtion back when we call it
+const socketio = require('socket.io') 
 
 const Filter = require('bad-words')
 const {gene,geneM} = require('./utils/messages')
@@ -24,39 +24,14 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// io.on('connection',()=>{
-//     console.log('New Websocket Connection') // now we have to initialize the client sideserver to interact
-// })
+
 
 let cnt = 0;
 io.on('connection',(socket)=>
 {
-     console.log('New Websocket Connection') // now we have to initialize the client sideserver to interact
-    
-     // socket.emit('countupdated',cnt)
-    
-    // socket.on('increment',()=>{ // for receiving the data from server end by suing socket.on
-    //     cnt++
-    //     // socket.emit('countupdated',cnt) 
-    //     io.emit('countupdated',cnt) // now we passing the whole object to the client if we open new tab then we start with 0
-    // })
+     console.log('New Websocket Connection') 
 
-    // it on 21st video:-
-    
-
-
-
-
-
-
-    // 21st Video
-
-
-    // we have to emit message task by udemy
     socket.emit('Message',gene('Welcome!')); 
-    // it is old before the time is used
-        //    now if user enters and exit give the message
-        //    socket.broadcast.emit('Message',gene('A new user is added!'))
 
     socket.on('join',(option,callback)=>{
         const {error,user} = addUser({ id: socket.id, ...option})
@@ -133,26 +108,10 @@ io.on('connection',(socket)=>
     })
 
 })
-//now we send data from client to server by changes in index.html and chat.js
 
 
 server.listen(port,() =>{
     console.log(`Server is up on port ${port}!`)
 })
 
-//now we are adding start :- node src/index.js in package json
-
-// now we install socket.io
-// now we create http sever which we directly pass it to
-
-
-// now we use geoloaction to send our location
-
-
-//acknowledge the emit that it is send or recieved from both end
-
-
-// Now we use time in html,css,javascript
-
-
-//now i make chat.html for all the content and index.html is join page as of now
+// we are adding start :- node src/index.js in package json
